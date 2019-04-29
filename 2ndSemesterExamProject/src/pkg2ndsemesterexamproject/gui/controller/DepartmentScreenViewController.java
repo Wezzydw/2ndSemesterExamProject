a448b8908dcb8403f07e5f6c0a7bd527497af28c
mapoul <mpoulsen1989@gmail.com>
2019-04-29 13:25:49 +0200

    controller

--- 2ndSemesterExamProject/src/pkg2ndsemesterexamproject/gui/controller/DepartmentScreenViewController.java
+++ 2ndSemesterExamProject/src/pkg2ndsemesterexamproject/gui/controller/DepartmentScreenViewController.java
@@ -7,6 +7,7 @@ package pkg2ndsemesterexamproject.gui.controller;
 
 import com.jfoenix.controls.JFXTextField;
 import java.net.URL;
+import java.time.LocalDate;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -14,6 +15,7 @@ import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.AnchorPane;
+import pkg2ndsemesterexamproject.be.Department;
 
 /**
  * FXML Controller class
@@ -21,18 +23,19 @@ import javafx.scene.layout.AnchorPane;
  * @author andreas
  */
 public class DepartmentScreenViewController implements Initializable
-{
-
+{    
+    private Department currentDepartment;
+    
     @FXML
     private ComboBox<?> comboBox;
-    @FXML
-    private Label lblComboText;
     @FXML
     private Label lblDate;
     @FXML
     private JFXTextField txtSearchfield;
     @FXML
     private AnchorPane departmentAnchorPane;
+    @FXML
+    private Label lblText;
 
     /**
      * Initializes the controller class.
@@ -40,11 +43,17 @@ public class DepartmentScreenViewController implements Initializable
     @Override
     public void initialize(URL url, ResourceBundle rb)
     {
-        // TODO
+        LocalDate date = LocalDate.now();
+        lblDate.setText(date.toString());
+        
     }    
 
     @FXML
     private void searchBar(KeyEvent event) {
+    }
+    
+    public void setDepartment(Department department){
+        lblText.setText(department.getName());
     }
     
 }
