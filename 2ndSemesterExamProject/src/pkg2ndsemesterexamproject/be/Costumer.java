239238890eb5797a7485ca20927b76c9afafefd0
Wezzydw <44640169+Wezzydw@users.noreply.github.com>
2019-04-29 10:21:08 +0200

    Interfaces

---
+++ 2ndSemesterExamProject/src/pkg2ndsemesterexamproject/be/Costumer.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkg2ndsemesterexamproject.be;
+
+/**
+ *
+ * @author Wezzy Laptop
+ */
+public class Costumer {
+
+    private String name;
+
+    public Costumer(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
