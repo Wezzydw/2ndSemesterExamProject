2196bd04f0496f3b9f544b5e6a5c47c8fb9e2c4e
Wezzydw <44640169+Wezzydw@users.noreply.github.com>
2019-05-07 12:37:51 +0200

    spaces

--- 2ndSemesterExamProject/src/pkg2ndsemesterexamproject/bll/DataHandler.java
+++ 2ndSemesterExamProject/src/pkg2ndsemesterexamproject/bll/DataHandler.java
@@ -1,8 +1,10 @@
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+
 package pkg2ndsemesterexamproject.bll;
 
 import java.util.ArrayList;
@@ -15,61 +17,10 @@ import pkg2ndsemesterexamproject.be.IWorker;
  *
  * @author Wezzy Laptop
  */
+
 public class DataHandler implements IDataHandler {
 
-    List<IProductionOrder> allProductionOrders;
-
     public DataHandler() {
-        allProductionOrders = new ArrayList();
+        
     }
-
-    @Override
-    public List<IProductionOrder> searchAllProductionOrders(String searchString) {
-
-        List<IProductionOrder> toReturn = new ArrayList();
-        loop:
-        for (IProductionOrder ipo : allProductionOrders) {
-            for (IDepartmentTask idt : ipo.getDepartmentTasks()) {
-                if (idt.getDepartment().getName().contains(searchString)) {
-                    toReturn.add(ipo);
-                    continue loop;
-                }
-
-                if (idt.getEndDate().toString().contains(searchString)) {
-                    toReturn.add(ipo);
-                    continue loop;
-                }
-                if (idt.getStartDate().toString().contains(searchString)) {
-                    toReturn.add(ipo);
-                    continue loop;
-                }
-
-                for (IWorker workers : idt.getActiveWorkers()) {
-                    if (workers.getName().contains(searchString) || workers.getInitials().contains(searchString)) {
-                        if (!toReturn.contains(idt)) {
-                            toReturn.add(ipo);
-                            continue loop;
-                        }
-                    }
-                }
-
-            }
-
-            if (ipo.getCustomer().getName().contains(searchString)) {
-                toReturn.add(ipo);
-                continue loop;
-            }
-            if (ipo.getDelivery().getDeliveryTime().toString().contains(searchString)) {
-                toReturn.add(ipo);
-                continue loop;
-            }
-            if (ipo.getOrder().getOrderNumber().contains(searchString)) {
-                toReturn.add(ipo);
-                continue loop;
-            }
-
-        }
-        return toReturn;
-    }
-
 }
