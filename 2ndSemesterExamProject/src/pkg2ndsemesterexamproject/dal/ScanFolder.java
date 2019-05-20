4779e8c520e9d51fdf217e12a40defb2a942a739
mapoul <mpoulsen1989@gmail.com>
2019-05-20 12:46:29 +0200

    scanFolder

---
+++ 2ndSemesterExamProject/src/pkg2ndsemesterexamproject/dal/ScanFolder.java
@@ -0,0 +1,77 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkg2ndsemesterexamproject.dal;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author mpoul
+ */
+public class ScanFolder {
+
+    private List<String> allFiles;
+
+    public ScanFolder() throws IOException {
+        allFiles = loadSavedFile();
+    }
+
+    public void updateFiles() throws IOException {
+        File files = new File("data/");
+        File[] listOfFiles = files.listFiles();
+        List<String> filePaths = new ArrayList<>();
+        for (File file : listOfFiles) {
+            if (file.isFile()) {
+                if ((file.getName().endsWith(".xlsx") || file.getName().endsWith(".csv") || file.getName().endsWith(".txt")) && !hasFileBeenSaved(file.getAbsolutePath())) {
+                    filePaths.add(file.getPath());
+                    writeSavedFile(file.getPath());
+                    allFiles.add(file.getPath());
+                }
+            }
+        }
+        
+    }
+
+    public List<String> loadSavedFile() throws FileNotFoundException, IOException {
+        FileReader fileReader = new FileReader(new File("data/readfiles.txt"));
+        BufferedReader br = new BufferedReader(fileReader);
+        List<String> readFile = new ArrayList();
+        String line;
+
+        while ((line = br.readLine()) != null) {
+            readFile.add(line);
+        }
+        return readFile;
+    }
+
+    public void writeSavedFile(String linesToWrite) throws IOException {
+        FileWriter writer = new FileWriter(new File("data/readfiles.txt"), true);
+        BufferedWriter bw = new BufferedWriter(writer);
+        bw.newLine();
+        bw.append(linesToWrite);
+        bw.close();
+    }
+
+    public boolean hasFileBeenSaved(String filepaths) throws IOException {
+        List<String> filesToRead = allFiles;
+        for (String string : filesToRead) {
+            if (string.equals(filepaths)) {
+                if(!filepaths.equals("readfiles.txt")){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
