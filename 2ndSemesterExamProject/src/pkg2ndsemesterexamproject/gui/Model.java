31c0d0d6a7e1c5b41d0eedca30e1411d26970ac8
Wezzydw <44640169+Wezzydw@users.noreply.github.com>
2019-05-21 11:25:59 +0200

    Doubleclick added

--- 2ndSemesterExamProject/src/pkg2ndsemesterexamproject/gui/Model.java
+++ 2ndSemesterExamProject/src/pkg2ndsemesterexamproject/gui/Model.java
@@ -77,7 +77,7 @@ public class Model {
     private String searchString = "";
     private ISortStrategy strategy;
     private double scalePost = 1;
-    
+
     public Model() throws IOException {
         stickyNotes = new ArrayList();
         ptl = new PassThrough();
@@ -145,13 +145,13 @@ public class Model {
         Label startDate = new Label(dpt.getStartDate().format(DateTimeFormatter.ofPattern("d/MM/YYYY")));
         Label endDate = new Label(dpt.getEndDate().format(DateTimeFormatter.ofPattern("d/MM/YYYY")));
         Pane orderPane = new Pane();
-        orderPane.setMaxSize(200*scale, 150*scale);
+        orderPane.setMaxSize(200 * scale, 150 * scale);
         orderPane.getStyleClass().add("pane");
-        Rectangle rec = new Rectangle(200*scale, 150*scale);
+        Rectangle rec = new Rectangle(200 * scale, 150 * scale);
         rec.setArcHeight(25);
         rec.setArcWidth(25);
         orderPane.setShape(rec);
-        Circle circle = new Circle(13*scale);
+        Circle circle = new Circle(13 * scale);
         circle.setFill(Paint.valueOf("Green"));
 
         for (int i = 0; i < tasks.size(); i++) {
@@ -164,76 +164,78 @@ public class Model {
         } else {
             circle.setFill(Paint.valueOf("Red"));
         }
-        
+
         orderNum.getStyleClass().add("label");
         customer.getStyleClass().add("label");
         startDate.getStyleClass().add("label");
         endDate.getStyleClass().add("label");
-        
-        orderNum.setStyle("-fx-font-size:"+ 15*scale);
-        customer.setStyle("-fx-font-size:"+ 15*scale);
-        startDate.setStyle("-fx-font-size:"+ 15*scale);
-        endDate.setStyle("-fx-font-size:"+ 15*scale);
-        
+
+        orderNum.setStyle("-fx-font-size:" + 15 * scale);
+        customer.setStyle("-fx-font-size:" + 15 * scale);
+        startDate.setStyle("-fx-font-size:" + 15 * scale);
+        endDate.setStyle("-fx-font-size:" + 15 * scale);
+
         Pane progress = new Pane();
-        progress.setMaxSize(175*scale, 15*scale);
+        progress.setMaxSize(175 * scale, 15 * scale);
         Canvas canvas = new Canvas();
-        canvas.setHeight(15*scale);
-        canvas.setWidth(175*scale);
-        canvas.setLayoutX(13*scale);
-        canvas.setLayoutY(130*scale);
+        canvas.setHeight(15 * scale);
+        canvas.setWidth(175 * scale);
+        canvas.setLayoutX(13 * scale);
+        canvas.setLayoutY(130 * scale);
 
         GraphicsContext gc = canvas.getGraphicsContext2D();
 
         Long daysBetween = ChronoUnit.DAYS.between(dpt.getStartDate(), dpt.getEndDate());
-        int progressInterval = (int) ((175*scale) / daysBetween);
+        int progressInterval = (int) ((175 * scale) / daysBetween);
         LocalDateTime todayIs = LocalDateTime.now();
         Long startToNow = ChronoUnit.DAYS.between(dpt.getStartDate(), todayIs);
         double dd = progressInterval * startToNow;
         gc.setFill(Color.GREEN);
-        if (progressInterval * startToNow > 175*scale) {
+        if (progressInterval * startToNow > 175 * scale) {
             gc.setFill(Color.RED);
-            dd = 175*scale;
+            dd = 175 * scale;
         }
-        gc.fillRect(0, 0, dd, 20*scale);
+        gc.fillRect(0, 0, dd, 20 * scale);
 //        gc.fillRect(0, 0, 160, 15);
         gc.setStroke(Color.BLACK);
-        gc.strokeRect(0, 0, 175*scale, 15*scale);
+        gc.strokeRect(0, 0, 175 * scale, 15 * scale);
 
         orderPane.getChildren().add(canvas);
 
-        circle.setLayoutX(180*scale);
-        circle.setLayoutY(20*scale);
+        circle.setLayoutX(180 * scale);
+        circle.setLayoutY(20 * scale);
 
-        orderNum.setLayoutX(50*scale);
-        orderNum.setLayoutY(15*scale);
+        orderNum.setLayoutX(50 * scale);
+        orderNum.setLayoutY(15 * scale);
 
-        startDate.setLayoutX(20*scale);
-        startDate.setLayoutY(100*scale);
+        startDate.setLayoutX(20 * scale);
+        startDate.setLayoutY(100 * scale);
 
-        orderNum.setLayoutX(25*scale);
-        orderNum.setLayoutY(40*scale);
+        orderNum.setLayoutX(25 * scale);
+        orderNum.setLayoutY(40 * scale);
 
-        customer.setLayoutX(25*scale);
-        customer.setLayoutY(60*scale);
+        customer.setLayoutX(25 * scale);
+        customer.setLayoutY(60 * scale);
 
-        startDate.setLayoutX(15*scale);
-        startDate.setLayoutY(100*scale);
+        startDate.setLayoutX(15 * scale);
+        startDate.setLayoutY(100 * scale);
 
-        endDate.setLayoutX(115*scale);
-        endDate.setLayoutY(100*scale);
+        endDate.setLayoutX(115 * scale);
+        endDate.setLayoutY(100 * scale);
 
-        progress.setLayoutX(5*scale);
-        progress.setLayoutY(130*scale);
-        
-        invis.setLayoutX(200*scale);
-        invis.setLayoutY(150*scale);
-        
+        progress.setLayoutX(5 * scale);
+        progress.setLayoutY(130 * scale);
+
+        invis.setLayoutX(200 * scale);
+        invis.setLayoutY(150 * scale);
+
         EventHandler<MouseEvent> event1 = (MouseEvent e)
                 -> {
-
-            goToOverview(po, dpt);
+            if (e.getClickCount() == 2) {
+                goToOverview(po, dpt);
+            }
         };
+
         orderPane.setOnMousePressed(event1);
 
         orderPane.getChildren().addAll(circle, orderNum, startDate, endDate, customer, invis);
@@ -241,13 +243,14 @@ public class Model {
         return orderPane;
 
     }
-    
-    public void zoomControl(double sliderVal){
-        double scale = sliderVal/100;
+
+    public void zoomControl(double sliderVal) {
+        double scale = sliderVal / 100;
         orderPaneHeigth = 150 * scale;
         orderPaneWidth = 200 * scale;
         scalePost = scale;
     }
+
     /*
     skifter fra det generelle overview over alle departmenttask til en specific 
     departmenttask.
@@ -425,5 +428,4 @@ public class Model {
         searchString = string;
     }
 
-    
 }
--- 2ndSemesterExamProject/src/pkg2ndsemesterexamproject/gui/controller/DepartmentScreenViewController.java
+++ 2ndSemesterExamProject/src/pkg2ndsemesterexamproject/gui/controller/DepartmentScreenViewController.java
@@ -196,9 +196,9 @@ public class DepartmentScreenViewController implements Initializable {
 
 //        departmentAnchorPane.setCursor(Cursor.V_RESIZE);
         if (lastDrag > event.getSceneY() && lastDrag > 0) {
-            scrollValue = scrollValue + 0.1;
+            scrollValue = scrollValue + 0.01;
         } else if (lastDrag < event.getSceneY() && lastDrag > 0) {
-            scrollValue = scrollValue - 0.1;
+            scrollValue = scrollValue - 0.01;
         }
         if (scrollValue < 0) {
             scrollValue = 0;
