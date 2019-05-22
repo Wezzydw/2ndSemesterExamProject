b87d60bb027d127949580312b96ea46b82cdff0d
andr4229 <andr4229@hotmail.com>
2019-05-22 10:24:44 +0200

    startScreen

--- 2ndSemesterExamProject/src/pkg2ndsemesterexamproject/gui/controller/StartScreenController.java
+++ 2ndSemesterExamProject/src/pkg2ndsemesterexamproject/gui/controller/StartScreenController.java
@@ -24,11 +24,12 @@ import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.MenuButton;
 import javafx.scene.control.MenuItem;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
-import javafx.stage.WindowEvent;
 import pkg2ndsemesterexamproject.be.IDepartment;
 
 /**
@@ -39,32 +40,42 @@ import pkg2ndsemesterexamproject.be.IDepartment;
 public class StartScreenController implements Initializable {
     
     @FXML
-    private MenuButton MenuButton;
-    
+    private MenuButton menuButton;
     private Model model;
-    
     private List<IDepartment> allDepartments;
 
     /**
      * Initializes the controller class.
+     * Den laver en ny model, og derefter prøver den at få alle
+     * departments fra model.getAllDepartments
+     * hvis den fanger en SQLExeption laver den et pop up vindue
+     * til sidst kaldes setUpMenuButtons
+     * @param url
+     * @param rb
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         try {
             model = new Model();
+            allDepartments = model.getAllDepartments();
         } catch (IOException ex) {
             Logger.getLogger(StartScreenController.class.getName()).log(Level.SEVERE, null, ex);
+            System.out.println("IOExeption: " + ex);
+        } catch (SQLException ex)
+        {
+            popUpScreen();
+            Logger.getLogger(StartScreenController.class.getName()).log(Level.SEVERE, null, ex);
         }
-        
-        try {
-            allDepartments = model.getAllDepartments();
-//        model.setMenuItems(MenuButton, model.getAllDepartments());
-        } catch (SQLException ex) {
-            System.out.println("ingen forbindelse");
-        }
-        
-        EventHandler<ActionEvent> event1 = (ActionEvent e)
-                -> {
+        setUpMenuButtons();
+    }   
+    /**
+     * Denne metode laver et event og laver menuItems
+     * for hver department der er, disse Items gives onAction den
+     * event som er blevet lavet, til sidst bliver alle knapperne 
+     * tilføjet til menuButton
+     */
+    private void setUpMenuButtons(){
+        EventHandler<ActionEvent> event1 = (ActionEvent e) -> {
             Department temp;
             MenuItem selectedItem = (MenuItem) e.getSource();
             String selectedItemName = selectedItem.getText();
@@ -76,7 +87,6 @@ public class StartScreenController implements Initializable {
             }
         };
         List<MenuItem> departmentBtns = new ArrayList();
-        
         if (allDepartments != null) {
             allDepartments.add(new Department("Manager"));
             for (IDepartment depar : allDepartments) {
@@ -84,24 +94,24 @@ public class StartScreenController implements Initializable {
                 item.setOnAction(event1);
                 departmentBtns.add(item);
             }
-        } else {
-            allDepartments = new ArrayList();
-            allDepartments.add(new Department("Manager"));
-            allDepartments.add(new Department("halv"));
-            allDepartments.add(new Department("bælg"));
-            for (IDepartment depar : allDepartments) {
-                MenuItem item = new MenuItem(depar.getName());//label skal være allDepartment.getName() fra BE laget 
-                item.setOnAction(event1);
-                departmentBtns.add(item);
-            }
-        }
-        
-        MenuButton.getItems().addAll(departmentBtns);
-        
-    }
+        } 
+//        else {
+//            allDepartments = new ArrayList();
+//            allDepartments.add(new Department("Manager"));
+//            allDepartments.add(new Department("halv"));
+//            allDepartments.add(new Department("bælg"));
+//            for (IDepartment depar : allDepartments) {
+//                MenuItem item = new MenuItem(depar.getName());//label skal være allDepartment.getName() fra BE laget 
+//                item.setOnAction(event1);
+//                departmentBtns.add(item);
+//            }
+//        }
+        menuButton.getItems().addAll(departmentBtns);
+    }   
 
-    /*
-    Denne metode skifter scene fra startscreen til manager screen
+    /**
+     * Denne metode skifter fra den nuværende FXML fil
+     * og kører ManagerOverview FXML og dens controller
      */
     private void goToManagerScreen() {
         FXMLLoader loader = new FXMLLoader();
@@ -116,13 +126,17 @@ public class StartScreenController implements Initializable {
         Parent p = loader.getRoot();
         Stage stage = new Stage();
         stage.setScene(new Scene(p));
-        Stage stage1 = (Stage) MenuButton.getScene().getWindow();
-//        stage1.close();
+        Stage stage1 = (Stage) menuButton.getScene().getWindow();
         stage.showAndWait();
+//        stage1.close();
     }
 
-    /*
-    Metoden skifter scene fra startscreen til DepartmentScreenViewet
+    /**
+     * Denne metodes skifter fra den nuværende FXML fil
+     * og kører DepartmentScreenView FXML og dens controller
+     * Den kører metoden setDepartment() inde fra 
+     * DepartmentScreenViewController med department som parameter
+     * @param department 
      */
     private void goToDepartmentScreen(Department department) {
         FXMLLoader loader = new FXMLLoader();
@@ -137,26 +151,47 @@ public class StartScreenController implements Initializable {
         Parent p = loader.getRoot();
         Stage stage = new Stage();
         stage.setScene(new Scene(p));
-        Stage stage1 = (Stage) MenuButton.getScene().getWindow();
-//        stage1.close();
+        Stage stage1 = (Stage) menuButton.getScene().getWindow();
         stage.setFullScreen(true);
         stage.showAndWait();
+//        stage1.close();
     }
-    
+    /**
+     * Denne metode tager en department og ud fra dens navn 
+     * kører metoden goToManagerScreen() hvis dens navn er manager,
+     * ellers kører den metoden goToDepartmentScreen() med department 
+     * som en parameter
+     * @param department 
+     */
     private void selectDepartment(Department department) {
         if (department.getName().equals("Manager")) {
             goToManagerScreen();
         } else {
-//            Pane orderPane = model.createOrderInGUI();
-//        Scene scene = new Scene(orderPane, 200, 150);
-//        Stage primaryStage = new Stage();
-//        primaryStage.setTitle("Hello World!");
-//        primaryStage.setScene(scene);
-//        primaryStage.show();
-//            
             goToDepartmentScreen(department);
-        }
-        
+        }        
     }
-    
+    /**
+     * Denne metode laver et pop up vindue, 
+     * som fortæller at der ikke er forbindelse til databasen
+     */
+    private void popUpScreen(){
+        Button btnOk = new Button("Ok");
+
+        Label lblNoConnection = new Label("There is no connection to the DB");
+        lblNoConnection.setLayoutX(40);
+        lblNoConnection.setLayoutY(80);
+        btnOk.setLayoutX(100);
+        btnOk.setLayoutY(125);
+
+        Pane root2 = new Pane();
+        root2.getChildren().addAll(btnOk, lblNoConnection);
+
+        Scene scene = new Scene(root2, 300, 250);
+        Stage primaryStage = new Stage();
+        primaryStage.setScene(scene);
+        btnOk.setOnAction((ActionEvent event) -> {
+           primaryStage.close();
+        });
+        primaryStage.show();
+    }
 }
