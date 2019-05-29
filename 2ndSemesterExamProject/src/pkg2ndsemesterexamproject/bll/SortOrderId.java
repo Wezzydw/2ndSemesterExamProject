b37e76ba9643232fa74d532414bc84992f05e3b5
mapoul <mpoulsen1989@gmail.com>
2019-05-29 10:56:47 +0200

    javadoc

--- 2ndSemesterExamProject/src/pkg2ndsemesterexamproject/bll/SortOrderId.java
+++ 2ndSemesterExamProject/src/pkg2ndsemesterexamproject/bll/SortOrderId.java
@@ -21,7 +21,15 @@ public class SortOrderId implements ISortStrategy, Comparator<IProductionOrder>
 {
 
     private String departmentName;
-
+    
+    /**
+     * Bruger klassens compare metode til at sortere listen af ProductionOrder 
+     * efter OrderId: lastet -> højest.
+     * @param list Listen af ProductionOrders der skal sorteres.
+     * @param departmentName Kommer fra den Department forespørgsel bliver lavet.
+     * @return en sorterer en liste af ProductionOrder i numerisk rækkefølge
+     * efter OrderId. Lavest til højest
+     */
     @Override
     public List<IProductionOrder> sort(List<IProductionOrder> list, String departmentName)
     {
@@ -30,7 +38,14 @@ public class SortOrderId implements ISortStrategy, Comparator<IProductionOrder>
         Collections.sort(list1, new SortOrderId());
         return list1;
     }
-
+    
+    /**
+     * Sammenligner OrderId fra 2 objekter af ProductionOrder.
+     * @param o1 ProductionOrder objekt 1
+     * @param o2 ProductionOrder objekt 2
+     * @return en int-værdi alt efter om OrderId i o1 kommer før eller
+     * efter OrderId i o2.
+     */
     @Override
     public int compare(IProductionOrder o1, IProductionOrder o2)
     {
